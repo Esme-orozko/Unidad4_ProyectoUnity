ca956508a3d4f8becef782bf7ff3abd18690073c
Esmeralda del Socorro Orozco Diaz <esmeralda.orozco24a@udgvirtual.udg.mx>
2026-04-07 18:43:31 -0600

    Actividad 4.1

---
+++ Unidad4_proyecto/Assets/Scripts/BallController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float impulseForce = 3f;
+    private bool ignoreNextCollision;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (ignoreNextCollision)
+        {
+            return;
+        }
+
+        rb.linearVelocity = Vector3.zero;
+        rb.AddForce(Vector3.up*impulseForce, ForceMode.Impulse);
+
+        ignoreNextCollision = true;
+        Invoke("AllownextCollision", 0.2f);
+    }
+
+    private void AllownextCollision()
+    {
+        ignoreNextCollision = false;
+    }
+
+}
